1eb1054cf417105761617b977811f13354730914
Shubham Chauhan <elskerelvish@outlook.com>
2025-04-22 19:39:49 +0530

    author column

--- jstechnova/settings.py
+++ jstechnova/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 # SECRET_KEY = "sdfads@#$%^&%ETRFDdfasda6"
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["www.jstechnova.in", "jstechnova.in"]
 
