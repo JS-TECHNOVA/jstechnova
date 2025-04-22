2f2a49ae2b02b55a0ff6eb31e1e6342da61ca793
Shubham Chauhan <elskerelvish@outlook.com>
2025-04-22 19:20:11 +0530

    author column

--- website/models.py
+++ website/models.py
@@ -100,12 +100,11 @@ STATUS_CHOICES = (
 ) 
 
 class Blogs(models.Model):
-
     created_at = models.DateTimeField(auto_now_add=True)
     title = models.CharField(max_length=200)
     image = models.ImageField(upload_to="uploads/blogs")
     content = models.TextField()
-    author = models.ForeignKey(User, null=True, blank=True, on_delete=models.SET_NULL)
+    # author = models.ForeignKey(User, null=True, blank=True, on_delete=models.SET_NULL)
 
     slug = models.SlugField(max_length = 250, null = True, blank = True) 
     updated = models.DateTimeField(auto_now = True) 
